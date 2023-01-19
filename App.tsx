a2790b1d970a993c9d84bbee40b1d53d70a4748d
gabrielimartins7 <martins.gabrieli.07.almeida@outlook.com>
2023-01-19 15:53:37 -0300

    configurando fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { 
+  useFonts, 
+  Inter_400Regular,
+  Inter_500Medium
+ } from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
 
 import { Home } from './src/screens/Home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  })
+
+  if(!fontsLoaded){
+    return null;
+  }
+
   return <Home />
 }
 
