41398494377150e38e01d336cd9755dc62c39a99
gabrielimartins <gabrieli.martins@housi.com.br>
2023-03-07 11:13:37 -0300

    Criando componente de acessorios

--- src/screens/carDetails/index.tsx
+++ src/screens/carDetails/index.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Accessory } from '../../components/Accessory';
+
+import speedSVG from '../../../assets/speed.svg';
+import accelerationSVG from '../../../assets/acceleration.svg';
+import forceSVG from '../../../assets/force.svg';
+import gasolineSVG from '../../../assets/gasoline.svg';
+import exchangeSVG from '../../../assets/exchange.svg';
+import peopleSVG from '../../../assets/people.svg';
+
+
 
 import { 
     Container,
@@ -15,7 +25,8 @@ import {
     Rent,
     Period,
     Price,
-    About
+    About,
+    Accessories
 } from './styles';
 
 export function CarDetails(){
@@ -44,6 +55,15 @@ export function CarDetails(){
                         <Price>R$ 580,00</Price>
                     </Rent>
                 </Details>
+
+                <Accessories>
+                    <Accessory name='380Km/h' icon={speedSVG} />
+                    <Accessory name='3.2s' icon={accelerationSVG} />
+                    <Accessory name='800 HP' icon={forceSVG} />
+                    <Accessory name='Gasolina' icon={gasolineSVG} />
+                    <Accessory name='Auto' icon={exchangeSVG} />
+                    <Accessory name='2 pessoas' icon={peopleSVG} />
+                </Accessories>
 
                 <About>
                     Este é automovel desportivo. Surgiu o lendário
--- src/screens/carDetails/styles.ts
+++ src/screens/carDetails/styles.ts
@@ -78,4 +78,16 @@ export const About = styled.Text`
     text-align: justify;
 
     margin-top: 23px;
+    line-height: ${RFValue(25)}px;
+`;
+
+export const Accessories = styled.View`
+    width: 100%;
+
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-top: 16px;
 `;
