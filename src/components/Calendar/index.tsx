66acd5115e8049cc9708a520e116c771b597b31c
gabrielimartins <gabrieli.martins@housi.com.br>
2023-03-14 20:49:18 -0300

    Finalizando componente de calendario

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 import { Feather } from '@expo/vector-icons'
 import { useTheme } from 'styled-components';
 
-import { Calendar as CustomCalendar } from 'react-native-calendars';
+import { 
+    Calendar as CustomCalendar,
+    LocaleConfig
+} from 'react-native-calendars';
+
+LocaleConfig.locales['pt-br'] = {
+    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    dayNames: ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'],
+    dayNamesShort: ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SÁB'],
+    today: "Hoje"
+  };
+  LocaleConfig.defaultLocale = 'pt-br';
 
 
 export function Calendar(){
@@ -17,6 +29,29 @@ export function Calendar(){
                     color={theme.colors.text}
                 />
             }
+
+            headerStyle={{
+                backgroundColor: theme.colors.background_secondary,
+                borderBottomWidth: 0.5,
+                borderBottomColor: theme.colors.text_detail,
+                paddingBottom: 10,
+                marginBottom: 10,
+            }}
+
+            theme={{
+                textDayFontFamily: theme.fonts.primary_400,
+                textDayHeaderFontFamily: theme.fonts.primary_500,
+                textDayHeaderFontSize: 10,
+                textMonthFontFamily: theme.fonts.secondary_600,
+                textMonthFontSize: 20,
+                monthTextColor: theme.colors.title,
+                arrowStyle: {
+                    marginHorizontal: -15
+                }
+            }}
+
+            firstDay={1}
+            minDate={new Date()}
         />
     );
 }
